b7a29fdf1940eaec7501b39d25e7eccaf71fcebf
Can H. Tartanoglu <gpg@rotas.mozmail.com>
2026-08-12 06:39:56 +0200

    chore: refresh flake inputs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Analyze Rust workspace layout by decomposing large workspaces into bounded analysis units";
 
   inputs = {
-    rs-harbor.url = "git+https://codeberg.org/caniko/rs-harbor.git?ref=trunk&rev=c26b735eede8078f795651c4a9cbf0be8733b221";
+    rs-harbor.url = "git+https://codefloe.com/caniko/rs-harbor.git?ref=trunk&rev=7fa1c2104dab4e1dbaa1aaa6df84bba815aa282d";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
@@ -46,7 +46,7 @@
       cargo-crosscut = buildCache.withRustCache { package = rustPlatform.buildRustPackage {
         pname = "cargo-crosscut";
         version = "0.1.0";
-        src = ./.;
+        src = pkgs.lib.cleanSource ./.;
         cargoLock.lockFile = ./Cargo.lock;
         meta.mainProgram = "cargo-crosscut";
       }; };
